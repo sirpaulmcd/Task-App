cb0b9372823be9e69d6545b28a9c2518da2ae607
Paul McDonald <sirpaulmcd@tutanota.com>
2021-05-06 13:30:39 -0600

    feat: Added routes for querying users and tasks

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const connectToDatabase = require("./db/mongoose");
 const User = require("./models/user");
+const Task = require("./models/task");
 
 const backendPort = process.env.BACKEND_PORT || 8000;
 
@@ -9,12 +10,76 @@ const app = express();
 
 app.use(express.json());
 
+// Get all users
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch(() => {
+      res.status(500).send();
+    });
+});
+
+// Get user by ID
+app.get("/users/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      return res.send(user);
+    })
+    .catch(() => {
+      return res.status(500).send();
+    });
+});
+
+// Create user
 app.post("/users", (req, res) => {
   const user = new User(req.body);
   user
     .save()
     .then(() => {
-      res.send(user);
+      res.status(201).send(user);
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    });
+});
+
+// Get all tasks
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.send(tasks);
+    })
+    .catch(() => {
+      res.status(500).send();
+    });
+});
+
+// Get task by ID
+app.get("/tasks/:id", (req, res) => {
+  Task.findById(req.params.id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+      return res.send(task);
+    })
+    .catch(() => {
+      return res.status(500).send();
+    });
+});
+
+// Create task
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task);
     })
     .catch((error) => {
       res.status(400).send(error);
