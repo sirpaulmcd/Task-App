dc1646546f8f735396fea8a64cb9ba6db0e8973e
Paul McDonald <sirpaulmcd@tutanota.com>
2021-05-06 14:14:12 -0600

    feat: Implemented update and delete routes for user and tasks

--- backend/src/index.js
+++ backend/src/index.js
@@ -44,6 +44,47 @@ app.post("/users", async (req, res) => {
   }
 });
 
+// Update user
+app.patch("/users/:id", async (req, res) => {
+  // Check that only valid properties are being updated
+  const updatedProperties = Object.keys(req.body);
+  const validProperties = ["name", "username", "email", "password"];
+  const updatedPropertiesAreValid = updatedProperties.every((property) => {
+    return validProperties.includes(property);
+  });
+  if (!updatedPropertiesAreValid) {
+    return res
+      .status(400)
+      .send({ error: "Attempted to update invalid property." });
+  }
+  // Update user and respond
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Delete user
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
 // Get all tasks
 app.get("/tasks", async (req, res) => {
   try {
@@ -75,6 +116,47 @@ app.post("/tasks", async (req, res) => {
     res.status(201).send(task);
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+
+// Update task
+app.patch("/tasks/:id", async (req, res) => {
+  // Check that only valid properties are being updated
+  const updatedProperties = Object.keys(req.body);
+  const validProperties = ["description", "completed"];
+  const updatedPropertiesAreValid = updatedProperties.every((property) => {
+    return validProperties.includes(property);
+  });
+  if (!updatedPropertiesAreValid) {
+    return res
+      .status(400)
+      .send({ error: "Attempted to update invalid property." });
+  }
+  // Update task and respond
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Delete task
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(500).send();
   }
 });
 
