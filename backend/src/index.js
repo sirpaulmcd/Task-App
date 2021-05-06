ab5d76e6ef674eeb5ab91496cfa91c7eb0c9c9a8
Paul McDonald <sirpaulmcd@tutanota.com>
2021-05-06 13:43:48 -0600

    refactor: updated routes to use async await

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,79 +11,71 @@ const app = express();
 app.use(express.json());
 
 // Get all users
-app.get("/users", (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.send(users);
-    })
-    .catch(() => {
-      res.status(500).send();
-    });
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
 // Get user by ID
-app.get("/users/:id", (req, res) => {
-  User.findById(req.params.id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send();
-      }
-      return res.send(user);
-    })
-    .catch(() => {
-      return res.status(500).send();
-    });
+app.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    return res.send(user);
+  } catch (error) {
+    return res.status(500).send();
+  }
 });
 
 // Create user
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 // Get all tasks
-app.get("/tasks", (req, res) => {
-  Task.find({})
-    .then((tasks) => {
-      res.send(tasks);
-    })
-    .catch(() => {
-      res.status(500).send();
-    });
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
 // Get task by ID
-app.get("/tasks/:id", (req, res) => {
-  Task.findById(req.params.id)
-    .then((task) => {
-      if (!task) {
-        return res.status(404).send();
-      }
-      return res.send(task);
-    })
-    .catch(() => {
-      return res.status(500).send();
-    });
+app.get("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    return res.send(task);
+  } catch (error) {
+    return res.status(500).send();
+  }
 });
 
 // Create task
-app.post("/tasks", (req, res) => {
-  const task = new Task(req.body);
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
+app.post("/tasks", async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 connectToDatabase()
