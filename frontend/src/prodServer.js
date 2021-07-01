0e846262db82a7fe5c4358984043fd4c25b60a63
Paul McDonald <sirpaulmcd@tutanota.com>
2021-07-01 17:45:59 -0600

    fix: updated prod server for client side routing

--- frontend/src/prodServer.js
+++ frontend/src/prodServer.js
@@ -5,7 +5,7 @@ const app = express();
 
 // When site is visited, serve production files from build directory
 app.use(express.static(path.join(__dirname, "../", "build")));
-app.get("/", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "../", "build", "index.html"));
 });
 
