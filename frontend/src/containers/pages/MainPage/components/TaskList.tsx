24df308ceab81a1462e362d9275e542527e6f633
Paul McDonald <sirpaulmcd@tutanota.com>
2021-07-03 18:14:20 -0600

    fix: changed list sorting to descending due date

--- frontend/src/containers/pages/MainPage/components/TaskList.tsx
+++ frontend/src/containers/pages/MainPage/components/TaskList.tsx
@@ -46,7 +46,7 @@ const TaskList: React.FC<TaskListProps> = () => {
       categoryParameter += `&category=${params.taskList}`;
     }
   }
-  let taskUrl = `${process.env.REACT_APP_BACKEND_URI}/tasks?sortBy=dueDateTime:asc${completionParameter}${categoryParameter}`;
+  let taskUrl = `${process.env.REACT_APP_BACKEND_URI}/tasks?sortBy=dueDateTime:desc${completionParameter}${categoryParameter}`;
 
   const getUserTasks = useCallback(async () => {
     await axios
